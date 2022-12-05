e074ca64f41fdb438347ded03db37117124c74d2
rostroh <romeostroh@gmail.com>
2022-12-05 13:24:27 +0100

    add level5

---
+++ level5/source.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+void	o(void)
+{
+	system("/bin/sh");
+	_exit(1);
+}
+
+void	n(void)
+{
+	char		buf[512];
+
+	fgets(buf, 512, stdin);
+	printf(buf);
+	exit(1);
+}
+
+int		main(void)
+{
+	n();
+}
